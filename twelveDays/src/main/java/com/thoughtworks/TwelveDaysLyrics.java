1e032ba571d348d91d23bf9a7581d548eca3e3c4
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-06-21 17:31:04 +0530

    Created the lyrics to the Twelve day of christmas

---
+++ twelveDays/src/main/java/com/thoughtworks/TwelveDaysLyrics.java
@@ -0,0 +1,14 @@
+package com.thoughtworks;
+
+public class TwelveDaysLyrics {
+
+    public static void main(String[] args) {
+
+        //Starter of the project to print Hello World
+        System.out.println("Hello, World");
+
+        //Created an object for Christmas class for instantiating
+        Christmas event = new Christmas();
+        event.lyrics();
+    }
+}
